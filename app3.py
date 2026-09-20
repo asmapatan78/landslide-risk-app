b73565a3c074f7af8190c6ae0ad6bd4579775d42
Patan Asma <asmapatan78@gmail.com>
2026-09-20 14:44:22 +0530

    Fix indentation for Historical data mode

--- app3.py
+++ app3.py
@@ -202,8 +202,7 @@ while True:
                 alert_sent = False
                 
         time.sleep(3)
-
-  elif data_mode == "Historical":
+elif data_mode == "Historical":
         with c2_live_placeholder.container():
             st.info("📅 Historical Mode Active")
             selected_date = st.date_input("Select Past Date:", value=None)
